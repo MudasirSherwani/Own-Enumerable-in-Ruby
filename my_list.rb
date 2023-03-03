0d14f21f8ba3b670beb18d8c8c324c1048a80faf
CHIMEZIE ASOUZU <shegy28@gmail.com>
2023-03-03 08:13:20 -0800

    Import my_enumerable using require

--- my_list.rb
+++ my_list.rb
@@ -1,7 +1,21 @@
+require_relative 'my_enumerable'
+
+class MyList
+  include MyEnumerable
+
+  def initialize(*list)
+    @list = list
+  end
+
+  def each(&block)
+    @list.each(&block)
+  end
+end
 
 list = MyList.new(1, 2, 3, 4)
-puts(list.all? { |e| e < 5 }) 
-puts(list.all? { |e| e > 5 }) 
-puts(list.any? { |e| e == 2 })
-puts(list.any? { |e| e == 5 })
-print(list.filter(&:even?))
+
+puts(list.all? { |e| e < 5 }) #=> true
+puts(list.all? { |e| e > 5 }) #=> false
+puts(list.any? { |e| e == 2 }) #=> true
+puts(list.any? { |e| e == 5 }) #=> false
+print(list.filter(&:even?)) #=> [2, 4]
