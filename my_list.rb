3c25f877ad8baa112853698ca72270c9ba74be37
Dell Pc <mudasir9757@gmail.com>
2023-03-03 20:38:15 +0500

    Add my_list

---
+++ my_list.rb
@@ -0,0 +1,7 @@
+
+list = MyList.new(1, 2, 3, 4)
+puts(list.all? { |e| e < 5 }) 
+puts(list.all? { |e| e > 5 }) 
+puts(list.any? { |e| e == 2 })
+puts(list.any? { |e| e == 5 })
+print(list.filter(&:even?))
